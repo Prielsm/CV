f4bb02a2890904f1d1ee1122be7f9dbc3285f635
Priels Manon <manon.priels@gmail.com>
2020-12-10 17:54:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+import './style/stars.sass';
+// import './style/fireflies.sass';
+import './style/main.scss';
+
+// Background Day Mode
+const quantity = 15;
+
+for (let index = 1; index < quantity; index++) {
+  document.body.innerHTML += '<div class="firefly"></div>';
+}
+
+// Boutons night/day mode
+const main = document.body.querySelector('main');
+console.log(main);
+const buttonsMode = document.body.querySelectorAll('.mode');
+
+buttonsMode.forEach((buttonMode) => {
+  buttonMode.addEventListener('click', (event) => {
+    if (event.currentTarget.id === 'night-mode') {
+      main.className = 'night';
+      console.log(main.className);
+    } else {
+      main.className = 'day';
+    }
+  });
+});
